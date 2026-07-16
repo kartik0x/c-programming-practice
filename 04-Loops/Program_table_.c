cf67a0f7088c04c7ccdfca919afffeac38a30d39
Kartik Gupta <kartikgupta.official01@gmail.com>
2026-07-17 01:14:14 +0530

    Add Program_table_.c solution

---
+++ 04-Loops/Program_table_.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    int num, i; 
+    printf(" Which Table Do You Want: ");
+    scanf("%d", &num);
+
+    for( i = 1; i <= 10 ; i++)
+    
+    {
+        printf("%d * %d = %d\n", num, i, num * i);
+    }
+    return 0;
+}
