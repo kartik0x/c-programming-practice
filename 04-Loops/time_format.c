3cbab9a8e55d8469853451f951dc319602f225db
Kartik Gupta <kartikgupta.official01@gmail.com>
2026-07-23 22:34:44 +0530

    add time_format.c solution

---
+++ 04-Loops/time_format.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+ int main ()
+ {
+    int hour;
+    for(hour = 0; hour < 24; hour++)
+    {
+        if(hour == 0)
+        {
+            printf("12 Midnight\n");
+            
+        } 
+        else if (hour < 12)
+        {
+            printf("%d AM\n", hour);
+
+        }else if(hour == 12)
+        {
+             printf("12 Noon\n");
+
+        }else 
+        printf("%d PM\n", hour - 12 );
+       
+        
+ 
+    }
+    return 0;
+    
+ }
